cba49ff2387ce246935db38770c8d1c4954feb85
DCouto11 <DCouto1197@gmail.com>
2025-05-26 09:05:08 +0000

    Initial version

---
+++ src/codification.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+from functions import clean_dataframe, transform_dataframe
+
+# Create spark session
+spark = SparkSession.builder \
+ .appName("CSV to Parquet") \
+ .getOrCreate()
+
+
+# Loading CSV files into DataFrames
+df_products = spark.read.option("header", "true").csv("files/products.csv")
+df_sales = spark.read.option("header", "true").csv("files/sales.csv")
+df_stores = spark.read.option("header", "true").csv("files/stores.csv")
+
+# Cleaning DataFrames (removing nulls, duplicates, trimming strings)
+df_prod_clean = clean_dataframe(spark, df_products)
+df_sales_clean = clean_dataframe(spark, df_sales)
+df_stores_clean = clean_dataframe(spark, df_stores)
+
+# DataFrame transformations (sales aggregation, monthly sales insights)
+df_agg_sales, df_monthly_sales, df_enriched_sales = transform_dataframe(spark, df_prod_clean, df_sales_clean, df_stores_clean)
+
+# Saving DataFrames in CSV format
+df_agg_sales.write.mode("overwrite").csv("output/agg_sales.csv", header=True)
+
+# Saving DataFrames in Parquet format
+df_enriched_sales.write.partitionBy('category','transaction_date').mode("overwrite").parquet("output/enriched_sales.parquet")
+
+# Ending Spark session
+spark.stop()
---
+++ src/functions.py
@@ -0,0 +1,73 @@
+from pyspark.sql.functions import trim, col, month, year, udf
+from pyspark.sql.types import StringType
+
+def clean_dataframe(session, df):
+    """
+    Clean a Spark DataFrame by removing rows with null values and duplicates.
+    Args:
+        session (SparkSession): Spark session.
+        df (DataFrame): DataFrame to clean.
+
+    Returns:
+        df_clean: Clean DataFrame.
+    """
+
+    # Remove rows with null values in all columns
+    df = df.dropna(how='all')
+
+    # Remove duplicate rows
+    df = df.dropDuplicates()
+
+    # Trim whitespace from string columns
+    df = df.select([trim(col).alias(col) if isinstance(df.schema[col].dataType, StringType) else col for col in df.columns])
+    return df
+
+def categorize_products(col):
+    """
+    Categorize products based on their values.
+    Args:
+        col (Column): Column containing product prices.
+
+    Returns:
+        Column: New column with a label of 3 possible values.
+    """
+    return col.when(col < 20, "Low") \
+             .when((col >= 20) & (col <= 100), "Medium") \
+             .otherwise("High")
+
+def transform_dataframe(session, df_product, df_sales, df_store):
+    """
+    Transform a Spark DataFrame by cleaning it and applying additional transformations.
+    Args:
+        session (SparkSession): Spark session.
+        df_product (DataFrame): DataFrame with information related with products.
+        df_sales (DataFrame): DataFrame with information related with sales.
+        df_store (DataFrame): DataFrame with information related with stores.
+    Returns:
+        df_agg (DataFrame): Dataframe with the total revenue for each store and product category
+        df_monthly_insights (DataFrame): Dataframe with the quantity sold for each product category and month
+        df_enriched (Dataframe): Combined DataFrame with enriched information from products, sales, and stores.
+
+    Steps:
+        1. Unify all DataFrames into a single DataFrame.
+        2. Get specific columns for each DataFrame to return.
+        3. Integrate UDF function to implement new column.
+    
+    """
+    df_unified = df_sales.join(df_product, ['product_id'], how='left') \
+                         .join(df_store, ['store_id'], how='left')
+    df_unified = df_unified.withColumn("revenue", df_unified["quantity"] * df_unified["price"])
+
+    df_agg = df_unified.groupBy("store_id", "category").sum("revenue").alias("total_revenue")
+
+    df_monthly_insights = df_unified.select("transaction_date", "category", "quantity")
+    df_monthly_insights = df_monthly_insights.withColumn("month", month(df_monthly_insights["transaction_date"]))
+    df_monthly_insights = df_monthly_insights.withColumn("year", year(df_monthly_insights["transaction_date"]))
+    df_monthly_insights = df_monthly_insights.groupBy("year", "month", "category").sum("quantity").alias("total_quantity")
+
+    df_enriched = df_unified.select("transaction_id","store_name","location","product_name","category","quantity","transaction_date", "price")
+
+    categorize_products_udf = udf(categorize_products , StringType())
+    df_enriched = df_enriched.withColumn("price_category", categorize_products_udf(col("price")))
+
+    return df_agg, df_monthly_insights, df_enriched
