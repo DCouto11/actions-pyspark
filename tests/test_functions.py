e054116678bd87586271d3b59344a3f76d615480
DCouto11 <DCouto1197@gmail.com>
2025-05-26 20:06:08 +0200

    Adjustment of expected df schemas

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,7 +2,7 @@ import chispa
 import pytest
 import datetime
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType, FloatType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType, FloatType, DoubleType
 
 from src.functions import (clean_dataframe,transform_dataframe)
 
@@ -64,19 +64,40 @@ def test_transform_dataframe():
     # Expected DataFrame after transformation
     expected_data_agg = [(1,"Category 1", 1250.00), 
                          (1,"Category 2", 2700.00)]
-    expected_df_agg = spark.createDataFrame(expected_data_agg, ["store_id", "category", "total_revenue"])
+    schema_expected_agg = StructType([
+        StructField("store_id", IntegerType(), True),
+        StructField("category", StringType(), True),
+        StructField("total_revenue", DoubleType(), True)
+    ])
+    expected_df_agg = spark.createDataFrame(expected_data_agg, schema=schema_expected_agg)
 
     expected_data_month = [(2025, 1, "Category 1", 10), 
                            (2025, 2, "Category 2", 20),
                            (2025, 2, "Category 1", 5),
                            (2025, 1, "Category 2", 10)]
-    expected_df_month = spark.createDataFrame(expected_data_month, ["year", "month", "category", "total_quantity"])
+    schema_expected_month = StructType([
+        StructField("year", IntegerType(), True),
+        StructField("month", IntegerType(), True),
+        StructField("category", StringType(), True),
+        StructField("total_quantity", DoubleType(), True)
+    ])
+    expected_df_month = spark.createDataFrame(expected_data_month, schema=schema_expected_month)
 
     expected_data_enrich = [(1,"Test","Avenue Test 3295", "Product A", "Category 1", 10, datetime.date(2025,1,1), 50.00,"Medium"),
                             (2,"Test","Avenue Test 3295", "Product B", "Category 2", 20, datetime.date(2025,2,1), 10.00,"Low"),
                             (3,"Test","Avenue Test 3295", "Product A", "Category 1",  5, datetime.date(2025,2,1),150.00,"High"),
                             (4,"Test","Avenue Test 3295", "Product B", "Category 2", 10, datetime.date(2025,1,1),250.00,"High")]
-    expected_df_enrich = spark.createDataFrame(expected_data_enrich, ["store_id", "category", "total_revenue"])
+    schema_expected_enrich = StructType([
+        StructField("transaction_id", IntegerType(), True),
+        StructField("store_name", StringType(), True),
+        StructField("location", StringType(), True),
+        StructField("product_name", StringType(), True),
+        StructField("quantity", IntegerType(), True),
+        StructField("transaction_date", DateType(), True),
+        StructField("price", FloatType(), True),
+        StructField("price_category", StringType(), True)
+    ])
+    expected_df_enrich = spark.createDataFrame(expected_data_enrich,schema=schema_expected_enrich)
 
     # Assert that the transformed DataFrame matches the expected DataFrame
     chispa.assert_df_equality(df_agg, expected_df_agg, ignore_row_order=True)
